5d12586585a87201c9fc66cf3661e0f0c827ea27
github username <github email address>
2019-10-14 10:51:59 -0500

    sort_working

--- mtv-frontend/src/components/Artist/list.js
+++ mtv-frontend/src/components/Artist/list.js
@@ -10,15 +10,15 @@ state = {
 }
 
   render() {
-    const compare = (a, b) => {
-      if ( a.id < b.id ){
-        return -1;
-      }
-      if ( a.id > b.id ){
-        return 1;
-      }
-      return 0;
-    };
+    // const compare = (a, b) => {
+    //   if ( a.id < b.id ){
+    //     return -1;
+    //   }
+    //   if ( a.id > b.id ){
+    //     return 1;
+    //   }
+    //   return 0;
+    // };
     const sortbyname = (a, b) => {
       if ( a.name < b.name ){
         return -1;
--- mtv-frontend/src/components/genre/Links.js
+++ mtv-frontend/src/components/genre/Links.js
@@ -2,16 +2,30 @@ import React from 'react';
 import { Route, Link, Switch, Redirect } from "react-router-dom";
 import ArtistList from '../Artist/list';
 
-const Genrelinks = ({ data }) => (
-  <div className="links">
+const Genrelinks = ({ data }) => {
+
+  function handleClick(e) {
+    e.preventDefault();
+    
+    console.log('The link was clicked.');
+  }
+
+  return <div className="links">
     <ul>
       {data && data.map(items => (
         <li key={items.id}>
           <Link to={`/genres/${items.id}`}>{items.title}</Link>
         </li>
       ))}
+      <form>
+  <label>
+    Comment:
+    <input type="text" comment="comment" />
+  </label>
+  <input onClick={handleClick} type="submit" value="Submit" />
+</form>
     </ul>
   </div>
-)
+}
 
 export default Genrelinks;
